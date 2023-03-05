053543a6a61d5cca4a76371528b060ce7ba80bd2
Jeff Hwang <wodud6359@gmail.com>
2023-03-05 21:26:37 +0900

    Section9

--- Section 09 - Javascript/index.js
+++ Section 09 - Javascript/index.js
@@ -1,1 +1,75 @@
-window.alert("Hello World!");
+// 1. print
+// window.alert("Hello World!");
+
+// 2. Variables
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+
+// var name = "Your name is " + firstName + ' ' + lastName + '.';
+
+// window.alert(name);
+
+// 3. Variables Exercise
+// function test() {
+//     var a = "3";
+//     var b = "8";
+
+//     // Exercise
+//     var tempA = a;
+//     var tempB = b;
+
+//     a = tempB;
+//     b = tempA;
+
+//     // var tempVariable = a;
+
+//     // a = b;
+//     // b = tempVariable;
+
+//     console.log("a is " + a);
+//     console.log("b is " + b);
+// }
+
+// 4. String
+// var message = "Hello";
+// alert(message + " " + "there!");
+
+// 5. length
+// var name = "Jeff";
+// var length = name.length;
+// alert("Your name has " + length);
+
+// var getData = prompt("Enter your characters!");
+// var lengthData = getData.length;
+
+// alert("Your data has " + lengthData);
+
+// 6. Slicing and Extracting parts of a string
+// var name = "Jeff Hwang";
+// var firstName = name.slice(0,4);
+// var lastName = name.slice(5,);
+
+// var name = "Your name is " + firstName + ' ' + lastName + '.';
+
+// window.alert(name);
+
+// var name = prompt("Enter your name");
+// var space = name.indexOf(" ");
+
+// var firstName = name.slice(0,parseInt(space));
+// var lastName = name.slice(space+1, );
+
+// var name = "Your name is " + firstName + ' ' + lastName + '.';
+
+// window.alert(name.toUpperCase());
+
+// 7. Numbers
+// var dogAge = prompt("How old your dog?");
+// var humanAge = (dogAge-2) * 4 + 21;
+
+// var str = "Your dog is " + humanAge + "years old in human years,";
+
+// window.alert(str);
+
+
+// 8. Function 
