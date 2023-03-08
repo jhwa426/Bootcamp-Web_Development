e77e8551bf943e547560f1babeb7496767e25ade
Jeff Hwang <wodud6359@gmail.com>
2023-03-08 11:00:26 +0900

    Section13

--- Section 13 - Advanced Javascript and DOM Manipulation/index.js
+++ Section 13 - Advanced Javascript and DOM Manipulation/index.js
@@ -5,10 +5,24 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numberOfDrumButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         
-        var buttonInnerHTML = this.innerHTML;
         // What to do when click detected.
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+        
+        buttonAnimation(buttonInnerHTML);
     });
 }
+
+
+// Detecting Keyboad Press
+document.addEventListener("keypress", function(event) {
+
+    makeSound(event.key);
+
+    buttonInnerHTML(event.key);
+});
+
 
 function makeSound(key) {
     switch (key) {
@@ -52,5 +66,14 @@ function makeSound(key) {
 }
 
 
+function buttonAnimation(currentKey) {
 
+    var activeButton = document.querySelector("." + currentKey);
 
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+      }, 100);
+      
+}
