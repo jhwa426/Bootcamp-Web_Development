7ff97a563695503611ca7a25b8e2ff53924756b8
Jeff Hwang <wodud6359@gmail.com>
2023-03-09 10:27:35 +0900

    Section14

--- Section 14 - jQuery/index.js
+++ Section 14 - jQuery/index.js
@@ -25,6 +25,8 @@ $("a").attr("href", "https://www.naver.com");
      
 //  });
 
+// Same function above
+// jQeury method
 
 // JS method
  for(var i = 0; i<5; i++) {
@@ -34,8 +36,80 @@ $("a").attr("href", "https://www.naver.com");
  }
 
 
-// Same function above
-// jQeury method
  $("button").select(function () { 
     $("h1").css("color", "purple");
  });
+
+
+
+ $("input").keypress(function (event) { 
+   $("h1").text(event.key);
+ });
+
+
+ $("input").keypress(function (event) { 
+   $("h1").text(event.key);
+ });
+
+
+
+ $("h1").on("click", function () {
+   $("h1").css("color", "purple");
+ });
+
+// jQuery before and after method
+$("h1").before("<button>before</button>");
+$("h1").after("<button>after</button>");
+
+
+// Custom animation
+
+// $("button").on("click", function () {
+//    $("h1").show();
+//  });
+
+
+// $("h1").on("click", function () {
+//    $("h1").hide();
+//  });
+
+
+
+
+// // fadeIn
+// $("button").on("click", function () {
+//    $("h1").fadeIn();
+//  });
+
+
+// // fadeOut
+//  $("h1").on("click", function () {
+//    $("h1").fadeOut();
+//  });
+
+
+
+// slideDown
+// $("button").on("click", function () {
+//    $("h1").slideDown();
+//  });
+
+
+// slideUp
+//  $("h1").on("click", function () {
+//    $("h1").slideUp();
+//  });
+
+
+
+// animate
+// $("button").on("click", function () {
+//    $("h1").animate({opacity: 0.5});
+//  });
+
+
+ // animate slideUp, slideDown animate
+ $("button").on("click", function () {
+   $("h1").slideUp().slideDown().animate({opacity: 0.5});
+ });
+
