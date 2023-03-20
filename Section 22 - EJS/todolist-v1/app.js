d4c1859e57659f2ae78ebeb391d435a66af04d54
Jeff Hwang <wodud6359@gmail.com>
2023-03-20 18:14:20 +0900

    Section22

--- Section 22 - EJS/todolist-v1/app.js
+++ Section 22 - EJS/todolist-v1/app.js
@@ -1,4 +1,15 @@
+// EJS is a simple templating language that lets you generate HTML markup with plain JavaScript.
 // npm install express body-parser nodemon
+
+// var : has the function or global scope.
+
+// let : has the block scope.
+
+// const : has the block scope.
+// - security
+// - thread safety
+// - reduce human mistake
+
 
 
 const express = require('express');
@@ -7,25 +18,78 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// creates collection
+let items = ["Buy food", "Coding", "Study"];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
 
 
 app.get('/', function (req, res) {
     
-    var today = new Date();
-    var currentDay = today.getDate();
+    let today = new Date();
+    let currentDay = today.getDay();
 
-    if(currentDay === 6 || currentDay === 0) {
-        res.write("It's weekend!");
-    } else {
-        res.sendFile(__dirname + "/index.html");
-    }
+    //// less than 5 (do if statement) more than 5 (do switch statement)
 
+    // switch(currentDay) {
+    //     case 0:
+    //         day = "Sunday";
+    //         break;
+    //     case 1:
+    //         day = "Monday";
+    //         break;
+    //     case 2:
+    //         day = "Tuesday";
+    //         break;
+    //     case 3:
+    //         day = "Wednesday";
+    //         break;
+    //     case 4:
+    //         day = "Thursday";
+    //         break;
+    //     case 5:
+    //         day = "Friday";
+    //         break;   
+    //     case 6:
+    //         day = "Saturday";
+    //         break;
 
+    //     default:
+    //         console.log("Error: current day is equal to " + currentDay);
+    // }
 
+    let options = { 
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
+
+    let day = today.toLocaleDateString("en-NZ", options);
+
+    res.render("list", {
+        kindOfDay: day,
+        newListItems: items
+    });
 
 });
 
 
+app.post('/', function (req, res) {
+    let item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+
+});
 
 
 
