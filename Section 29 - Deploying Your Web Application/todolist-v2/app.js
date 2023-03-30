1dabc440aefe454457a3e6986298293f656e9aeb
Jeff Hwang <wodud6359@gmail.com>
2023-03-30 13:44:54 +0900

    Section28

---
+++ Section 29 - Deploying Your Web Application/todolist-v2/app.js
@@ -0,0 +1,194 @@
+//jshint esversion:6
+// npm install init
+// npm install mongoose@5.3.4 --> // current mongoose version is 7.0.3 but we use 5.3.4
+// npm install lodash
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { mongo } = require('mongoose');
+const _ = require("lodash");
+
+// const date = require(__dirname + "/date.js");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+
+// ------ DB area -----------
+
+
+
+// Connect db server and create db
+mongoose.connect("mongodb+srv://wodud6359:testbytest@cluster0.upzbgb5.mongodb.net/todolistDB", {useNewUrlParser: true}); // create or connect if does not exist, it creates automatically
+
+// Connect DB server (mongo DB Atlas)
+// Connect to Cluster0
+// mongo "mongodb+srv://cluster0.upzbgb5.mongodb.net/myFirstDatabase" --username wodud6359
+
+
+
+// Create new schema
+const itemSchema = new mongoose.Schema({
+    name : String
+});
+
+// Create a collection (mongo model uses capital letter)
+const Item = mongoose.model("Item", itemSchema);
+
+// Create a new table
+const item1 = new Item ({
+    name : "Welcome to your To - do List"
+});
+
+const item2 = new Item ({
+    name: "Add the to-do object below"
+});
+
+const item3 = new Item ({
+    name: "Ex : Coding"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+// list Schema for dynamic route
+
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+
+
+
+// ------ DB area -----------
+
+
+
+app.get("/", function (req, res) {
+
+    Item.find({}, function(err, foundItems) {
+
+      if (foundItems.length === 0) {
+        // insert Many 
+        Item.insertMany(defaultItems, function(err) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("Successfully inserted items!");
+            }
+        });
+        res.redirect("/");
+      } else {
+          res.render("list", { listTitle: "Today", newListItems: foundItems });
+      }
+    });
+});
+
+
+
+// dynamic route -- important!!!! that no more use like /about, /work
+app.get("/:customListName", function(req, res) {
+    const customListName = _.capitalize(req.params.customListName);
+
+    List.findOne({name: customListName}, function(err, foundList) {
+        if(!err) {
+            if(!foundList) {
+
+              // create a new list
+              const list = new List({
+                name: customListName,
+                items: defaultItems
+              });
+
+              list.save();
+              res.redirect("/" + customListName);
+
+            } else {
+
+              // Show an existing list
+              res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+            }
+        }
+    });
+});
+
+
+
+
+
+app.post("/", function (req, res) {
+
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
+
+    const item = new Item({
+        name: itemName
+    });
+
+    if (listName === "Today") {
+        item.save();
+        res.redirect("/");
+    } else {
+        List.findOne({name: listName}, function(err, foundList) {
+          foundList.items.push(item);
+          foundList.save();
+          res.redirect("/" + listName);
+        });
+    }
+
+});
+
+
+
+
+// delete route
+app.post("/delete", function (req, res) {
+
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemId, function(err) {
+            if (!err) {
+              console.log("Successfully deleted selected items!");
+              res.redirect("/");
+            }
+        });
+      } else {
+          List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList) {
+              if(!err) {
+                  res.redirect("/" + listName);
+              }
+          });
+      }
+
+  });
+
+
+
+  // /work
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+
+// /about 
+app.get("/about", function (req, res) {
+    res.render("about");
+});
+
+// those two are not using anymore as a dynamic route is created
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
