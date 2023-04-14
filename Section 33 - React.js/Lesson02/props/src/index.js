ff91c0e9d9c9e039689a711dc6218a72823f4d96
Jeff Hwang <wodud6359@gmail.com>
2023-04-14 15:31:13 +0900

    Section33

--- Section 33 - React.js/Lesson02/props/src/index.js
+++ Section 33 - React.js/Lesson02/props/src/index.js
@@ -1,49 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function Card(props) {
-    return (
-        <div className='my-style'>
-            <h2 className='card-name'>{props.name}</h2>
-            <img
-                src={props.img}
-                alt="avatar_img"
-            />
-            <p>{props.tel}</p>
-            <p>{props.email}</p>
-        </div >
-    );
-}
-
-ReactDOM.render(
-    <div>
-        <h1>My Contacts</h1>
-
-        <Card
-            name="Beyonce"
-            img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            tel="+123 456 789"
-            email="b@beyonce.com"
-        />
-
-        <input id="fName" placeholder="Enter your first name." value="Jeff Hwang" />
-
-        <Card
-            name="Jack Bauer"
-            img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-            tel="+987 654 321"
-            email="jack@nowhere.com"
-
-        />
-
-        <Card
-            name="Chuck Norris"
-            img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-            tel="+918 372 574"
-            email="gmail@chucknorris.com"
-        />
+ReactDOM.render(<App />, document.getElementById("root"));
 
 
-    </div>,
-    document.getElementById("root")
-);
+
+// function Card(props) {
+//     return (
+//         <div className='my-style'>
+//             <h2 className='card-name'>{props.name}</h2>
+//             <img
+//                 src={props.img}
+//                 alt="avatar_img"
+//             />
+//             <p>{props.tel}</p>
+//             <p>{props.email}</p>
+//         </div >
+//     );
+// }
+
+// ReactDOM.render(
+//     <div>
+//         <h1>My Contacts</h1>
+
+//         <Card
+//             name="Beyonce"
+//             img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+//             tel="+123 456 789"
+//             email="b@beyonce.com"
+//         />
+
+//         <input id="fName" placeholder="Enter your first name." value="Jeff Hwang" />
+
+//         <Card
+//             name="Jack Bauer"
+//             img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+//             tel="+987 654 321"
+//             email="jack@nowhere.com"
+
+//         />
+
+//         <Card
+//             name="Chuck Norris"
+//             img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+//             tel="+918 372 574"
+//             email="gmail@chucknorris.com"
+//         />
+
+
+//     </div>,
+//     document.getElementById("root")
+// );
