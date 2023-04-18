816066de935adc23d2315b869ab26ce6fd6afeb5
Jeff Hwang <wodud6359@gmail.com>
2023-04-18 11:11:51 +0900

    Section33

--- Section 33 - React.js/Lesson02/props/src/components/App.jsx
+++ Section 33 - React.js/Lesson02/props/src/components/App.jsx
@@ -3,6 +3,20 @@ import Card from './Card';
 import contacts from '../contacts';
 import Avatar from './Avatar';
 
+
+function createdCard(contact) {
+    return (
+        <Card
+            id={contact.id}
+            key={contact.id} // this is not for use // must be default rule
+            name={contact.name}
+            img={contact.imgURL}
+            tel={contact.phone}
+            email={contact.email}
+        />
+    );
+}
+
 function App() {
     return (
         <div>
@@ -11,7 +25,11 @@ function App() {
                 img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
             />
 
-            <Card
+            {contacts.map(createdCard)}
+
+
+
+            {/* <Card
                 name="Beyonce"
                 img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
                 tel="+123 456 789"
@@ -24,7 +42,7 @@ function App() {
                 img={contacts[1].imgURL}
                 tel={contacts[1].phone}
                 email={contacts[1].email}
-            />
+            /> */}
 
 
 
