3f4bc503876f36b8e94913cf4e37b154a48fbcac
Jeff Hwang <wodud6359@gmail.com>
2023-04-19 16:31:04 +0900

    Section33

--- Section 33 - React.js/Lesson03/prac-componets/src/components/App.jsx
+++ Section 33 - React.js/Lesson03/prac-componets/src/components/App.jsx
@@ -2,23 +2,48 @@ import React from "react";
 import Entry from './Entry';
 import emojipedia from '../emojipedia';
 
-function App() {
-  return (
-    <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
-
-      <dl className="dictionary">
+function showData(emojiTerm) {
+    return (
         <Entry
-          emoji="💪"
-          name="Tense Biceps"
-          description=" You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+            key={emojiTerm.id}
+            emoji={emojiTerm.emoji}
+            name={emojiTerm.name}
+            description={emojiTerm.meaning}
         />
+    );
+}
 
-      </dl>
-    </div>
-  );
+
+// const minData = emojipedia.map(function (emojiTerm) {
+//     return emojiTerm.meaning.substring(0, 100);
+// });
+
+function App() {
+    return (
+        <div>
+            <h1>
+                <span>emojipedia</span>
+            </h1>
+
+            <dl className="dictionary">
+                {emojipedia.map(showData)}
+
+
+                {/* arrow function of emojipedia*/}
+                {emojipedia.map(emojiTerm => (
+                    <Entry
+                        key={emojiTerm.id}
+                        emoji={emojiTerm.emoji}
+                        name={emojiTerm.name}
+                        description={emojiTerm.meaning}
+                    />
+                ))}
+
+
+
+            </dl>
+        </div>
+    );
 }
 
 export default App;
