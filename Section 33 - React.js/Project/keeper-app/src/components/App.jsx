4d6d7b142724cd8772fc4db0de3add9cccbec194
Jeff Hwang <wodud6359@gmail.com>
2023-04-20 11:48:23 +0900

    Section33

--- Section 33 - React.js/Project/keeper-app/src/components/App.jsx
+++ Section 33 - React.js/Project/keeper-app/src/components/App.jsx
@@ -2,13 +2,37 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from '../notes';
+
+// noteData function (normal)
+function noteData(item) {
+    return (
+        <Note
+            key={item.key}
+            title={item.title}
+            content={item.content}
+        />
+    );
+}
 
 function App() {
     return (
         <div>
             <Header />
 
-            <Note />
+            {/* <Note /> */}
+
+            {/* {notes.map(noteData)} */}
+
+            {/* arrow function */}
+
+            {notes.map(noteItem => (
+                <Note
+                    key={noteItem.key}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                />
+            ))}
 
             <Footer />
         </div>
