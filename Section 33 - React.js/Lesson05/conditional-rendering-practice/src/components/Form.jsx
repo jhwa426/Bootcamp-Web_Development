7b46083b16232cb100c8bfaa12c820660bc337d5
Jeff Hwang <wodud6359@gmail.com>
2023-04-24 14:07:54 +0900

    Section33

--- Section 33 - React.js/Lesson05/conditional-rendering-practice/src/components/Form.jsx
+++ Section 33 - React.js/Lesson05/conditional-rendering-practice/src/components/Form.jsx
@@ -6,8 +6,7 @@ function Form(props) {
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
 
-      {props.isRegistered === false && <input type="password" placeholder="Confirm Password" />
-      }
+      {props.isRegistered === false && <input type="password" placeholder="Confirm Password" />}
 
       <button type="submit">
         {props.isRegistered ? "Login" : "Register"}
