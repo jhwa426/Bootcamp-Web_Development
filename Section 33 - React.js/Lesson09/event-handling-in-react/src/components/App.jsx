dd6936d50c436a2a87394021ad2c46a4797c2122
Jeff Hwang <wodud6359@gmail.com>
2023-04-26 17:00:48 +0900

    Section33

--- Section 33 - React.js/Lesson09/event-handling-in-react/src/components/App.jsx
+++ Section 33 - React.js/Lesson09/event-handling-in-react/src/components/App.jsx
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+
+    const [headingText, settingText] = useState("Hello");
+    const [isMouseOver, setMouseOver] = useState(false);
+
+    function handleClick() {
+        settingText("Submitted");
+    }
+
+    function handleMouseOver() {
+        setMouseOver(true);
+    }
+
+    function handleMouseOut() {
+        setMouseOver(false)
+    }
+
+    function backToMain() {
+        settingText("Hello");
+    }
+
+    return (
+        <div className="container">
+            <h1>{headingText}</h1>
+            <input type="text" placeholder="What's your name?" />
+            <button
+                style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+                onClick={handleClick}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+            >
+                Submit</button>
+
+            {/* TEST AREA */}
+            <div className='back'>
+                <button
+                    style={{ backgroundColor: isMouseOver ? "red" : "white" }}
+                    onClick={backToMain}
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}
+
+                >
+                    Back</button>
+            </div>
+            {/* TEST AREA */}
+
+        </div>
+    );
 }
 
 export default App;
