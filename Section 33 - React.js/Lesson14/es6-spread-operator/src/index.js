47ed3077de42fdfffabc7ceab675a691efd2ca1d
Jeff Hwang <wodud6359@gmail.com>
2023-05-11 14:00:27 +0900

    Section33

--- Section 33 - React.js/Lesson14/es6-spread-operator/src/index.js
+++ Section 33 - React.js/Lesson14/es6-spread-operator/src/index.js
@@ -3,3 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// ...Spread operator
+
+// const citrus = ["Lime", "Lemon", "Orange"];
+// const fruits = ["Apple", ...citrus, "Banana", "Coconut"]; // ...citrus -> "Apple", "Lime", "Lemon", "Orange", "Banana", "Coconut"
+
+// const fullName = {
+//     fName: "Jeff",
+//     lName: "Hwang"
+// }
+
+// const user = {
+//     ...fullName,
+//     id: 1,
+//     username: "jhwa426"
+// }
+// //     fName: "Jeff", lName: "Hwang", id: 1, username: "jhwa426"
